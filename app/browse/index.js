dba9db6be9380a0bb90ae449555d84f5bba60c54
jaketrent <trent.jake@gmail.com>
2016-01-28 07:22:53 -0700

    Split container out of browse; panes styles

--- app/browse/index.js
+++ app/browse/index.js
@@ -1,55 +1,74 @@
 import autobind from 'autobind-decorator'
 import connect from 'redux-react-connect-by-name'
 import { Link } from 'react-router'
+import styleable from 'react-styleable'
 import React, { Component } from 'react'
 
 import * as actions from './actions'
 import * as directoryUtils from './directory'
-import styles from './index.css'
+import css from './index.css'
+
+const { func } = React.PropTypes
+
+function setDirectoryAttr(input) {
+  if (input && typeof input.setAttribute === 'function')
+    input.setAttribute('webkitdirectory', 'webkitdirectory')
+}
+
+function Browse(props) {
+  return (
+    <div className={props.css.root}>
+      <div className={props.css.panes}>
+        <div className={props.css.pane}>
+          <label htmlFor="directory">
+            <div>Select a source directory</div>
+            <input type="file"
+                   id="directory"
+                   multiple="multiple"
+                   ref={setDirectoryAttr}
+                   placeholder="Choose Directory"
+                   onChange={props.onChangeSrcDir} />
+          </label>
+        </div>
+        <div className={props.css.pane}>
+          <label htmlFor="destination">
+            <div>Select a destination directory</div>
+              <input type="file"
+                     id="destination"
+                     ref={setDirectoryAttr}
+                     placeholder="Choose Directory"
+                     onChange={props.onChangeDestDir} />
+           </label>
+        </div>
+      </div>
+
+      <Link to="filter">Go to filter</Link>
+    </div>
+  )
+}
+
+Browse.propTypes = {
+  onChangeSrcDir: func.isRequired,
+  onChangeDestDir: func.isRequired
+}
+
+const StyledBrowse = styleable(css)(Browse)
 
 @connect([], [actions])
 @autobind
-export default class Browse extends Component {
-  handleChooseDirectory(evt) {
+export default class BrowseContainer extends Component {
+  handleChooseSrcDir(evt) {
     const f = evt.target.files.item(0)
     this.props.browse.setDirectory(directoryUtils.create(f))
   }
-  handleChooseDestination(evt) {
+  handleChooseDestDir(evt) {
     const f = evt.target.files.item(0)
     this.props.browse.setDestination(directoryUtils.create(f))
   }
-  setDirectoryAttr(input) {
-    if (input && typeof input.setAttribute === 'function')
-      input.setAttribute('webkitdirectory', 'webkitdirectory')
-  }
   render() {
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Find images</h2>
-          <form>
-            <label htmlFor="directory">
-              <div>Select Directory</div>
-              <input type="file"
-                     id="directory"
-                     multiple="multiple"
-                     ref={this.setDirectoryAttr}
-                     placeholder="Choose Directory"
-                     onChange={this.handleChooseDirectory} />
-            </label>
-            <label htmlFor="destination">
-              <div>Select Destination</div>
-                <input type="file"
-                       id="destination"
-                       ref={this.setDirectoryAttr}
-                       placeholder="Choose Directory"
-                       onChange={this.handleChooseDestination} />
-             </label>
-          </form>
-
-          <Link to="filter">Go to filter</Link>
-        </div>
-      </div>
+      <StyledBrowse onChangeDestDir={this.handleChooseSrcDir}
+                    onChangeSrcDir={this.handleChooseDestDir} />
     )
   }
 }
